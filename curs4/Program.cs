0e498f7b403f8e69cab1adeac6c950cc7c873627
YONDER\AndreiP <andrei.pirv@tss-yonder.com>
2017-10-26 08:57:24 +0300

    Created the project

---
+++ curs4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curs4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
